ae3ba302a3b543710deb8138747317a26206e52b
shikku <131972005+shikku1@users.noreply.github.com>
2023-06-16 23:34:54 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+{
+  const btn = document.getElementById('btn');
+  const result = document.getElementById('result');
+
+  btn.addEventListener('click', () => {
+    const results = ['大吉','中吉','小吉','末吉'];
+    const n = Math.floor(Math.random() * results.length);
+
+    result.textContent = results[n];
+  });
+}
